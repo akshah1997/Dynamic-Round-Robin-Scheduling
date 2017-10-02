1db57b95cf4262f5becee8f3a3a851983aee1294
Akshay Shah <akshah1997@gmail.com>
2017-10-02 18:51:21 +0530

    Calculating Burst Time and Turnaround Time
    Displaying the burst and turnaround times of each process along with the average burst and turnaround time.

--- DynamicRoundRobin.java
+++ DynamicRoundRobin.java
@@ -8,7 +8,7 @@ class DynamicRoundRobin {
 
   static Process p[];
   static ArrayList<Process> p1 = new ArrayList<Process>(0);
-  static int index=0, timeCount;
+  static int index=0, timeCount, mainCount;
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -38,8 +38,7 @@ class DynamicRoundRobin {
     }
 
     Arrays.sort(p);
-    int brst[] = new int[no_of_proc];
-    int ind = 0;
+    mainCount = p[0].getArrivalTime();
 
     for(timeCount=0; ; ) {
       checkForProcess(timeCount, index);
@@ -49,20 +48,26 @@ class DynamicRoundRobin {
 
       if(flag) {
         maxBurstTime = p2.get(p2.size()-1).getBurstTime();
-        System.out.println(maxBurstTime);
+        //System.out.println(maxBurstTime);
         timeQuantum = 0.8f * maxBurstTime;
-        System.out.println("Time Quantum now: " + timeQuantum);
+        System.out.println("\n\nTime Quantum now: " + timeQuantum);
       }
 
-      for(int i=0; i<p1.size(); i++)
-        System.out.print((p1.get(i).getProcessNo()+1)+"\t");
+      /*for(int i=0; i<p1.size(); i++)
+        System.out.print((p1.get(i).getProcessNo()+1)+"\t");*/
 
-      timeCount += p1.get(0).getBurstTime();
-      System.out.println("\n");
+      /*p1.get(0).setWaitingTime(timeCount-p1.get(0).getArrivalTime());
+      timeCount += p1.get(0).getBurstTime();*/
+
+      //System.out.println("\n");
       for(int i=0; i<p1.size(); i++) {
         if(p1.get(i).getBurstTime() <= timeQuantum && !p1.get(i).isFinish()) {
-          System.out.println((p1.get(i).getProcessNo()+1)+"\n");
+          System.out.print("Process finished: "+(p1.get(i).getProcessNo()+1)+"\n");
+          p1.get(i).setWaitingTime(mainCount-p1.get(i).getArrivalTime());
+          timeCount += p1.get(i).getBurstTime();
+          mainCount += p1.get(i).getBurstTime();
           p1.get(i).setFinish(true);
+          p1.get(i).setTurnaroundTime(mainCount-p1.get(i).getArrivalTime());
           v.removeElement(i+1);
         }
       }
@@ -78,9 +83,21 @@ class DynamicRoundRobin {
       }
     }
 
+    System.out.println("\n\nProcess\t\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time");
+    for(int i=0; i<no_of_proc; i++) {
+      System.out.println("   "+p[i].getProcessNo()+"\t\t   "+p[i].getArrivalTime()+"\t\t   "+p[i].getBurstTime()+"\t\t   "+p[i].getWaitingTime()+"\t\t  "+p[i].getTurnaroundTime());
+      avgWaitingTime += p[i].getWaitingTime();
+      avgTurnaroundTime += p[i].getTurnaroundTime();
+    }
+
+    avgWaitingTime /= no_of_proc;
+    avgTurnaroundTime /= no_of_proc;
+    System.out.println("\nAverage Waiting Time: "+avgWaitingTime);
+    System.out.println("Average Turnaround Time: "+avgTurnaroundTime);
+
   }
 
-  public static boolean checkForProcess(int time, int ind)
+  public static void checkForProcess(int time, int ind)
   {
     for(int j=ind; j<p.length; j++)
     {
@@ -91,8 +108,8 @@ class DynamicRoundRobin {
       }
     }
     if(index > ind)
-      return true;
+      return;
     timeCount++;
-    return false;
+    return;
   }
 }
